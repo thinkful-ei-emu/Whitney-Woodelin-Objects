188845626098ddd24df948bc0bd366f29b46fef9
Whitney Wallace <whitneysamwallace@gmail.com>
2019-06-05 13:30:43 -0700

    added cracking the code base work

--- object-3.js
+++ object-3.js
@@ -97,3 +97,28 @@ if(storage[i].hasOwnProperty('boss')) {
  console.log(`${storage[i].jobTitle} ${storage[i].name} doesn't report to anybody.`);
 }
 
+//cracking the code
+// --------- 
+function decode(string) {
+  let newArray = string.split(' ');
+  let decodedSentence = [];
+
+  for(let i=0; i<newArray.length; i++) {
+    let firstLetter = newArray[i].charAt(0);
+    if(firstLetter === 'a') {
+      decodedSentence.push(newArray[i].slice(1, 2));
+    } else if(firstLetter === 'b') {
+      decodedSentence.push(newArray[i].slice(2, 3));
+      console.log(decodedSentence);
+    } else if(firstLetter === 'c') {
+      decodedSentence.push(newArray[i].slice(3, 4));
+    } else if(firstLetter === 'd') {
+      decodedSentence.push(newArray[i].slice(4, 5));
+    } else {decodedSentence.push(' ');}
+      
+  }
+  return decodedSentence.join().replace(/,/g, '');
+
+}
+decode('craft block argon meter bells brown croon droop');
+// ---------
