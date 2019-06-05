9f8729e6406f4981d49ac9fe37a759fc3011a0ff
woodelin <woodelin@woodelins-MBP.home>
2019-06-05 14:05:11 -0400

    great work

--- objecttwo.js
+++ objecttwo.js
@@ -13,3 +13,40 @@ function makeStudentsReport(data) {
   return results;
 }
 
+
+const studentData = [
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology',
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics',
+  },
+  {
+    name: 'Liz',
+    status: 'On leave',
+    course: 'Computer science',
+  },
+];
+
+function enrollInSummerSchool(students) {
+  
+  //enrollInSummerSchool should return an array of objects. For each object from the original array
+
+let newData = [];
+
+  for(i=0; i < students.length; i++){
+
+    const student = students[i];
+
+    newData.push({name: student.name, status:'In Summer school',course: student.course});
+
+
+  }
+
+  return newData;
+}
+
