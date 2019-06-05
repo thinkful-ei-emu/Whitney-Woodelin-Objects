e9d8f9c2f2776a9473b7faf4eb728aa73c291a96
Whitney Wallace <whitneysamwallace@gmail.com>
2019-06-05 13:04:23 -0700

    hobbit object added

--- object-3.js
+++ object-3.js
@@ -49,3 +49,14 @@ for(var property in obj){
   const key = property;
   console.log(key,value);
 }
+
+// Arrays in objects
+// Create an object with a property called meals which is an array of strings: 'breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper'.
+// Use console.log to show the name of a hobbit's fourth meal of the day.
+// Don't forget that humans and hobbits count from 1, but computers count from 0.
+
+const hobbitMeals = {
+  meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+};
+
+console.log(hobbitMeals.meals[3]);
