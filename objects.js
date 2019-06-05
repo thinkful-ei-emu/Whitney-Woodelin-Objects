7accafd558bb424df0669d6c8c0d0fcc6f47ce5e
woodelin <woodelin@woodelins-MBP.home>
2019-06-05 12:26:49 -0400

    great job

--- objects.js
+++ objects.js
@@ -32,3 +32,21 @@ function personMaker() {
   };
   return person;
 }
+
+
+
+
+
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+function keyDeleter(obj) {
+  // your code here
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
